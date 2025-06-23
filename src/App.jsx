33db9b7cd3023424dbbe07721a6384df3fb318be
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-06-23 10:13:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import "./App.css";
+import data from "./data/data.json";
+
+const Result = () => {
+  return (
+    <section className="result-section">
+      <h1>Your Result</h1>
+      <p>
+        <span>76</span>
+        of 100
+      </p>
+      <p>Great</p>
+      <p>
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </section>
+  );
+};
+
+const Summary = () => {
+  return (
+    <section className="summary-section">
+      <h2>Summary</h2>
+      <ResultList />
+      <button>Continue</button>
+    </section>
+  );
+};
+
+const ResultList = () => {
+  const listItems = data.map((item) => {
+    return (
+      <li key={item.id}>
+        <img src={item.icon} alt="" />
+        <p>{item.category}</p>
+        <p>
+          <span>{item.score}</span>
+        </p>
+      </li>
+    );
+  });
+
+  return (
+    <ul role="list">
+      {listItems}
+    </ul>
+  );
+};
+
+const App = () => {
+  return (
+    <main className="wrapper">
+      <Result />
+      <Summary />
+    </main>
+  );
+};
+
+export default App;
+
+//   Reaction
+//   80 / 100
+
+//   Memory
+//   92 / 100
+
+//   Verbal
+//   61 / 100
+
+//   Visual
+//   72 / 100
+
+//   Continue
